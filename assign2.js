2439fdb3b3ac077bc4c9bc098908f0abc74948f5
ManvirD <dhaliwalm50@yahoo.com>
2017-01-23 20:08:39 +0000

    first commit

---
+++ assign2.js
@@ -0,0 +1,13 @@
+var myVal1 = 5;
+var myVal2 = 10;
+
+var myVal3 = process.argv.length;
+Number(process.argv[myVal3]);
+
+var myVal4 = '15';
+console.log(myVal1,myVal2,myVal3,myVal4);
+
+Number(myVal3) + "<br>" +
+Number(myVal4) + "<br>" ;
+var sum = (+myVal1) + (+myVal2) + (+myVal3) + (+myVal4);
+console.log(sum);
