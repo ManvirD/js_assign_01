5adc4e9713484e8b465f4310de3fc2f9770770b2
ManvirD <dhaliwalm50@yahoo.com>
2017-01-23 20:04:04 +0000

    first commit

---
+++ assign1.js
@@ -0,0 +1,15 @@
+var myArr = [];
+myArr.push('one');
+console.log(myArr);
+
+myArr.unshift('two');
+console.log(myArr);
+
+myArr.push('three');
+console.log(myArr);
+
+myArr.pop();
+console.log(myArr);
+
+myArr.shift();
+console.log(myArr);
